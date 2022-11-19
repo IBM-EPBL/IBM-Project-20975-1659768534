f1689260d940bb6959d2c5bf01e6d3818237597d
Naveen-C07 <113403603+Naveen-C07@users.noreply.github.com>
2022-11-19 20:08:30 +0530

    Add files via upload

---
+++ Final Deliveries/Final code/static/js/app.js
@@ -0,0 +1,14 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+var uname = document.querySelector("#username").value;
+document.getElementById('login').innerHTML = "Hii"+""+uname;
